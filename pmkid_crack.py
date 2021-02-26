24f0491b5ff90e47fb0a68659c215bee8e0f97bf
Eda Abec <69101632+eda-abec@users.noreply.github.com>
2021-02-26 08:10:08 +0000

    Cleanup

--- pmkid_crack.py
+++ pmkid_crack.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import sys
 import subprocess
@@ -8,6 +9,8 @@ import datetime
 import os
 import re
 import codecs
+
+
 try:    import argparse
 except: print('argparse required, run: pip install argparse');     sys.exit(1)
 try:    import pexpect
@@ -17,7 +20,7 @@ except: print('netifaces required, run: pip install netifaces');    sys.exit(1)
 
 
 def get_time():
-    return datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d %H:%M:%S')
+    return datetime.datetime.strftime(datetime.datetime.now(),'[%m-%d %H:%M:%S]')
 
 
 # class taken from (and edited)
@@ -209,14 +212,14 @@ def print_usage():
     result  +=  "[-b] | [--bssid]      target BSSID"+"\n"
     result  +=  "[-e] | [--essid]      target ESSID"+"\n"
     result  +=  "[-t] | [--timeout]    max seconds to wait for a PMKID.         Defaults to 30"+"\n"
-    result  +=  "[-f] | [--file]       tmp file: wpa_supplicant and pmkid hash. Defaults to ./wpa_passphrase.cnf"+"\n"
+    result  +=  "[-f] | [--file]       tmp file: wpa_supplicant and PMKID hash. Defaults to ./wpa_passphrase.cnf"+"\n"
     result  +=  "[-c] | [--crack]      crack by haschat (or not).               Defaults to 'do-not-crack'"+"\n"
     result  +=  "[-d] | [--dictionary] dictionary file to be used by hashcat"+"\n"
     result  +=  "[-m] | [--mask]       hashcat mask"+"\n"
     result  +=  "[-p] | [--password]   foo password used by wpa_supplicant.     Defaults to 'spameggs'"+"\n"
     result  +=  "\n"
     result  +=  "If -c is specified, hashcat will be called. Otherwise, PMKID hash (if any) will just be displayed"+"\n"
-    result  +=  "If -c is specified, pmkid output hash will be stored in -f value, and -d or -m needs to be specified"+"\n"
+    result  +=  "If -c is specified, PMKID output hash will be stored in -f value, and -d or -m needs to be specified"+"\n"
     result  +=  "\n"
     result  +=  "\n"
     result  +=  "hashcat version cannot easily be infered. Depending on how it was installed, hashcat -V could display"+"\n"
@@ -226,38 +229,40 @@ def print_usage():
     result  +=  "    etc, so be sure your version is >= 4.2.0"+"\n"
     
     print(result)
+
+
 #-----------------------------------------------------------------------------------
 if(__name__=='__main__'):
     current_version =   '0.1.1'
     
     
-    unspecified     =   object()
-    argument_parser =   argparse.ArgumentParser(usage=None,add_help=False)
-    argument_parser.add_argument('-h','--help'      ,action='store_true',default=False                 ,dest='help'      ,required=False  )
-    argument_parser.add_argument('-v','--version'   ,action='store_true',default=False                 ,dest='version'   ,required=False  )
-    argument_parser.add_argument('-i','--interface' ,action='store'     ,default=None                  ,dest='iface'     ,required=True   )
-    argument_parser.add_argument('-b','--bssid'     ,action='store'     ,default=None                  ,dest='bssid'     ,required=False  )
-    argument_parser.add_argument('-e','--essid'     ,action='store'     ,default=None                  ,dest='essid'     ,required=False  )
-    argument_parser.add_argument('-t','--timeout'   ,action='store'     ,default=30                    ,dest='max_time'  ,required=False  )
-    argument_parser.add_argument('-c','--crack'     ,action='store_true',default=None                  ,dest='crack'     ,required=False  )
-    argument_parser.add_argument('-d','--dictionary',action='store'     ,default=None                  ,dest='dictionary',required=False  )
-    argument_parser.add_argument('-f','--file'      ,action='store'     ,default='./wpa_passphrase.cnf',dest='file'      ,required=False  )
-    argument_parser.add_argument('-m','--mask'      ,action='store'     ,default=None                  ,dest='mask'      ,required=False  )
-    argument_parser.add_argument('-p','--password'  ,action='store'     ,default='spameggs'            ,dest='password'  ,required=False  )
+    parser       = argparse.ArgumentParser(usage=None, add_help=False)
+    parser.add_argument('-h','--help'      ,action='store_true',default=False                 ,dest='help'      ,required=False)
+    parser.add_argument('-v','--version'   ,action='store_true',default=False                 ,dest='version'   ,required=False)
+    parser.add_argument('-i','--interface' ,action='store'     ,default=None                  ,dest='iface'     ,required=True )
+    parser.add_argument('-b','--bssid'     ,action='store'     ,default=None                  ,dest='bssid'     ,required=False)
+    parser.add_argument('-e','--essid'     ,action='store'     ,default=None                  ,dest='essid'     ,required=False)
+    parser.add_argument('-t','--timeout'   ,action='store'     ,default=30                    ,dest='max_time'  ,required=False)
+    parser.add_argument('-c','--crack'     ,action='store_true',default=None                  ,dest='crack'     ,required=False)
+    parser.add_argument('-d','--dictionary',action='store'     ,default=None                  ,dest='dictionary',required=False)
+    parser.add_argument('-f','--file'      ,action='store'     ,default='./wpa_passphrase.cnf',dest='file'      ,required=False)
+    parser.add_argument('-m','--mask'      ,action='store'     ,default=None                  ,dest='mask'      ,required=False)
+    parser.add_argument('-p','--password'  ,action='store'     ,default='spameggs'            ,dest='password'  ,required=False)
     
     
-    argument_parser_result      =   argument_parser.parse_args()
-    option_help                 =   argument_parser_result.help
-    option_version              =   argument_parser_result.version
-    iface                       =   argument_parser_result.iface
-    bssid                       =   argument_parser_result.bssid
-    essid                       =   argument_parser_result.essid
-    max_time                    =   argument_parser_result.max_time
-    tmp_file                    =   argument_parser_result.file
-    wpa_supplicant_password     =   argument_parser_result.password
-    crack                       =   argument_parser_result.crack
-    password_dictionary_file    =   argument_parser_result.dictionary
-    password_mask               =   argument_parser_result.mask
+    args                        =   parser.parse_args()
+    
+    option_help                 =   args.help
+    option_version              =   args.version
+    iface                       =   args.iface
+    bssid                       =   args.bssid
+    essid                       =   args.essid
+    max_time                    =   args.max_time
+    tmp_file                    =   args.file
+    wpa_supplicant_password     =   args.password
+    crack                       =   args.crack
+    password_dictionary_file    =   args.dictionary
+    password_mask               =   args.mask
 
     
     if(option_version):
@@ -293,7 +298,7 @@ if(__name__=='__main__'):
     # 1) call wpa_passphrase to generate wpa_supplicant file; with foo password
     parameter_list          =   ['wpa_passphrase', essid,wpa_supplicant_password, '>', tmp_file]
     parameter_list_string   =   ' '.join(parameter_list)
-    print(get_time() + ' calling wpa_passphrase as:\n' + parameter_list_string)
+    print('{} calling wpa_passphrase as:\n\t{}'.format(get_time(), parameter_list_string))
     try:    subprocess.check_output(parameter_list_string, shell=True)   # shel=True ===> arg must be string, not list
     except: pass
 
@@ -307,7 +312,7 @@ if(__name__=='__main__'):
     parameter_list          =   ['wpa_supplicant', '-c', tmp_file, '-i', iface, '-dd']
     parameter_list_string   =   ' '.join(parameter_list)
     cmd_output              =   None
-    print(get_time() + ' calling wpa_supplicant as:\n' + parameter_list_string)
+    print('{} calling wpa_supplicant as:\n\t{}'.format(get_time(), parameter_list_string))
     
     
     
@@ -316,20 +321,20 @@ if(__name__=='__main__'):
     child                   =   pexpect.spawn(parameter_list_string, timeout=int(max_time))
     try:
         child.expect('.*PMKID from Authenticator.*')
-        print(get_time()+' pmkid retrieved!')
+        print('{} PMKID retrieved!'.format(get_time()))
         child.sendcontrol('c')
         pmkid_found = True
-    except pexpect.exceptions.EOF as e:
-        print(get_time() + ' did not receive PMKID')
-        sys.exit(0)
-    except pexpect.exceptions.TIMEOUT as e:
-        print(get_time() + ' timeout')
+    except pexpect.exceptions.EOF:
+        print('{} did not receive PMKID'.format(get_time()))
+        sys.exit(2)
+    except pexpect.exceptions.TIMEOUT:
+        print('{} timeout'.format(get_time()))
         print("Note: you can change the limit with -t <seconds>")
-        sys.exit(0)
+        sys.exit(3)
     except KeyboardInterrupt:
         print("\nAborting…")
         sys.exit(0)
-    except Exception as e:
+    except Exception:
         pass
     if(pmkid_found == True):
         cmd_output = child.after
@@ -344,15 +349,14 @@ if(__name__=='__main__'):
         if('RSN: PMKID from Authenticator - hexdump' in current_line):
             hex_pmkid   =   current_line.split(':')[2].replace(' ','').upper()
             whole_hash  =   hex_pmkid + '*' + bssid_hex + '*' + iface_mac + '*' + essid_hex.decode('utf-8')
-            print('\n' + whole_hash + '\n')
+            print('\n{}\n'.format(whole_hash))
             break
-    time_pmkid_end      =   time.time()
-    pmkid_time_elapsed  =   time_pmkid_end - time_pmkid_start
-    print(get_time() + ' pmkid request finished in ' + '%.3f'%(pmkid_time_elapsed) + ' s')
     
-    if(pmkid_found == False or crack == None):
-        print(get_time() + ' DONE!')
-        sys.exit(0)
+    pmkid_time_elapsed  = time.time() - time_pmkid_start
+    print('{} PMKID request finished in {} s'.format(get_time(), '%.3f'%(pmkid_time_elapsed)))
+    
+    if pmkid_found == False or crack == None:
+        print('{} DONE!'.format(get_time()))
     else:
         # copy the pmkid whole hash to the -f file
         with open(tmp_file, 'w') as fd_hash_file:
@@ -360,5 +364,5 @@ if(__name__=='__main__'):
         print('TODO! should call hashcat such as')
         print('hashcat -m 16800 ' + tmp_file + ' <dictionary_file>|<mask>')
            
-        print(get_time() + ' DONE!')
+        print('{} DONE!'.format(get_time()))
 
